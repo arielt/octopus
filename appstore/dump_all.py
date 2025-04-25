508ceb80564d8363ffd772ca41d1cbf82c5f0ede
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-04-25 15:12:58 -0700

    app store: update dump all script

--- appstore/dump_all.py
+++ appstore/dump_all.py
@@ -2,28 +2,23 @@
 Driver script to dump app versions for all platforms.
 """
 
-from dataclasses import dataclass
+from google_play import scrape_zxbase_version
+from apple_app_store import AppleAppStore
 
 
-@dataclass
-class AppStoreApp:
+def main():
     """
-    Represents an app in the App Store.
+    Driver script to get the latest version of the app from all stores.
     """
-    platform: str
-    store: str
-    app_id: str
-    version: str = "Undetected"
+    print(f"{'Android':<10} {scrape_zxbase_version()}")
+
+    apple_app = AppleAppStore("zxbase", "1608405737")
+    print(f"{'iOS':<10} {apple_app.scrape_ios_version()}")
+    print(f"{'iPad':<10} {apple_app.scrape_ipad_version()}")
+    print(f"{'macOS':<10} {apple_app.scrape_macos_version()}")
+
+    # TBD: Windows, Linux
 
 
-apps = [
-    AppStoreApp(platform="Android", store="Google Play", app_id="com.example.app"),
-    AppStoreApp(platform="iOS", store="MacApp Store", app_id="com.example.app"),
-    AppStoreApp(platform="macOS", store="MacApp Store", app_id="com.example.app"),
-    AppStoreApp(platform="iPad", store="MacApp Store", app_id="com.example.app"),
-    AppStoreApp(platform="Windows", store="Microsoft Store", app_id="com.example.app"),
-    AppStoreApp(platform="Linux", store="Snapcraft", app_id="com.example.app"),
-]
-
-for app in apps:
-    print(f"{app.platform:<10} {app.version:<10}")
+if __name__ == "__main__":
+    main()
