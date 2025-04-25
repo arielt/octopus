1475ace6bfc6fe7b87a7395308bf0491e38491ee
I T <it@zxbase.com>
2025-04-25 13:37:27 -0700

    apple app store: address linter warnings

--- appstore/apple_app_store.py
+++ appstore/apple_app_store.py
@@ -1,5 +1,5 @@
 """
-Scrape public Apple App Store URL for the latest version of the app.
+Scrape public Apple App Store URL for the latest version of the app + driver script.
 """
 
 import requests
@@ -7,19 +7,28 @@ from bs4 import BeautifulSoup
 
 
 class AppleAppStore:
+    """
+    Scrape public Apple App Store URL for the latest version of the app.
+    """
     def __init__(self, app_name: str, app_id: str):
         self.app_name = app_name
         self.app_id = app_id
 
     def version(self, platform: str):
+        """
+        Get the latest version of the app for a given platform.
+        """
         url = f"https://apps.apple.com/us/app/{self.app_name}/id{self.app_id}?platform={platform}"
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)
         soup = BeautifulSoup(response.text, "html.parser")
         version = soup.find("p", class_="whats-new__latest__version").text.strip()
         return version
 
 
 def main():
+    """
+    Driver script to get the latest version of the app for all platforms.
+    """
     app = AppleAppStore("zxbase", "1608405737")
     print(f"{'iOS':<10} {app.version('ios')}")
     print(f"{'iPad':<10} {app.version('ipad')}")
