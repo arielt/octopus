9a4acd07c0c84a2533a9aadcc5582f7694d76d07
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-04-25 15:06:15 -0700

    app store: update mac app store scraping

--- appstore/apple_app_store.py
+++ appstore/apple_app_store.py
@@ -10,11 +10,12 @@ class AppleAppStore:
     """
     Scrape public Apple App Store URL for the latest version of the app.
     """
+
     def __init__(self, app_name: str, app_id: str):
         self.app_name = app_name
         self.app_id = app_id
 
-    def version(self, platform: str):
+    def scrape_version(self, platform: str):
         """
         Get the latest version of the app for a given platform.
         """
@@ -22,7 +23,25 @@ class AppleAppStore:
         response = requests.get(url, timeout=10)
         soup = BeautifulSoup(response.text, "html.parser")
         version = soup.find("p", class_="whats-new__latest__version").text.strip()
-        return version
+        return version.split(" ")[-1]
+
+    def scrape_ios_version(self):
+        """
+        Get the latest version of the app for iOS.
+        """
+        return self.scrape_version("ios")
+
+    def scrape_ipad_version(self):
+        """
+        Get the latest version of the app for iPad.
+        """
+        return self.scrape_version("ipad")
+
+    def scrape_macos_version(self):
+        """
+        Get the latest version of the app for macOS.
+        """
+        return self.scrape_version("macos")
 
 
 def main():
@@ -30,9 +49,9 @@ def main():
     Driver script to get the latest version of the app for all platforms.
     """
     app = AppleAppStore("zxbase", "1608405737")
-    print(f"{'iOS':<10} {app.version('ios')}")
-    print(f"{'iPad':<10} {app.version('ipad')}")
-    print(f"{'macOS':<10} {app.version('macos')}")
+    print(f"{'iOS':<10} {app.scrape_ios_version()}")
+    print(f"{'iPad':<10} {app.scrape_ipad_version()}")
+    print(f"{'macOS':<10} {app.scrape_macos_version()}")
 
 
 if __name__ == "__main__":
